e7b407ad069b242fab9c786c76887f37da16a92d
Yeezy2277 <40365400+Yeezy2277@users.noreply.github.com>
2021-02-06 16:35:51 +0300

    Adapted iPad

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ const text = document.querySelectorAll(".articles-slider__text");
 const investBlocks = document.querySelectorAll(".investers-block__item");
 const sliderLine = document.querySelector(".slider-line");
 const btnLeft = document.querySelector(".left-btn");
+const InvestersBtnLeft = document.querySelector(".investers-left-btn");
+const ExpertsBtnLeft = document.querySelector(".experts-left-btn");
 const btnRight = document.querySelector(".right-btn");
+const InvestersBtnRight = document.querySelector(".investers-right-btn");
+const ExpertsBtnRight = document.querySelector(".experts-right-btn");
+
+let investers1 = document.querySelector("#investers-1");
+let investers2 = document.querySelector("#investers-2");
+let investers3 = document.querySelector("#investers-3");
+
+let experts1 = document.querySelector("#expert-block-1");
+let experts2 = document.querySelector("#expert-block-2");
+let experts3 = document.querySelector("#expert-block-3");
+
 let count = 0;
+let countInvests = 0;
+let countExperts = 0;
 let width;
 // slider text
 const init = () => {
@@ -14,7 +29,21 @@ const init = () => {
 	item.style.width = width + 'px'
 	item.style.height = 'auto';
 	});
+	investers1.style.display = "block";
+	investers2.style.display = "block";
+	investers3.style.display = "block";
+	experts1.style.display = "block";
+	experts2.style.display = "block";
+	experts3.style.display = "block";
 	rollSlider();
+	if (window.innerWidth <= '915') {
+		investers1.style.display = "block";
+		investers2.style.display = "none";
+		investers3.style.display = "none";
+		experts1.style.display = "none";
+		experts2.style.display = "block";
+		experts3.style.display = "none";
+	}
 }
 window.addEventListener('resize', init);
 init();
@@ -53,5 +82,162 @@ document.querySelector(".burger").addEventListener("click", function(e) {
 		document.querySelector("#menu").classList.add("nav-active");
 		document.body.classList.add("body-active");
 	}
+});
+//
+// slider invests
+	InvestersBtnLeft.addEventListener('click', (pos) => {
+		countInvests--;
+		switch (countInvests) {
+			case 0: {
+				investers1.style.display = "block";
+				investers2.style.display = "none";
+				investers3.style.display = "none";
+				break;
+			}
+
+			case 1: {
+				investers1.style.display = "none";
+				investers2.style.display = "block";
+				investers3.style.display = "none";
+				break;
+			}
+
+			case 2: {
+				investers1.style.display = "none";
+				investers2.style.display = "none";
+				investers3.style.display = "block";
+				break;
+			}
+		}
+		if (countInvests > 3) {
+			investers1.style.display = "block";
+			investers2.style.display = "none";
+			investers3.style.display = "none";
+			countInvests = 0;
+		}
+
+		if (countInvests < 0) {
+			investers1.style.display = "none";
+			investers2.style.display = "none";
+			investers3.style.display = "block";
+			countInvests = 2;
+		} 
+	});
+	InvestersBtnRight.addEventListener('click', (pos) => {
+		countInvests++;
+		switch (countInvests) {
+			case 0: {
+				investers1.style.display = "block";
+				investers2.style.display = "none";
+				investers3.style.display = "none";
+				break;
+			}
+
+			case 1: {
+				investers1.style.display = "none";
+				investers2.style.display = "block";
+				investers3.style.display = "none";
+				break;
+			}
+
+			case 2: {
+				investers1.style.display = "none";
+				investers2.style.display = "none";
+				investers3.style.display = "block";
+				break;
+			}
+		}
+		if (countInvests > 3) {
+			investers1.style.display = "block";
+			investers2.style.display = "none";
+			investers3.style.display = "none";
+			countInvests = 0;
+		}
+
+		if (countInvests < 0) {
+			investers1.style.display = "none";
+			investers2.style.display = "none";
+			investers3.style.display = "block";
+			countInvests = 2;
+		} 
+	});
+//
+
+// slider Experts
+ExpertsBtnLeft.addEventListener('click', (pos) => {
+	countExperts--;
+	switch (countExperts) {
+		case 0: {
+			experts1.style.display = "none";
+			experts2.style.display = "block";
+			experts3.style.display = "none";
+			break;
+		}
+
+		case 1: {
+			experts1.style.display = "block";
+			experts2.style.display = "none";
+			experts3.style.display = "none";
+			break;
+		}
+
+		case 2: {
+			experts1.style.display = "none";
+			experts2.style.display = "none";
+			experts3.style.display = "block";
+			break;
+		}
+	}
+	if (countExperts > 3) {
+		experts1.style.display = "block";
+		experts2.style.display = "none";
+		experts3.style.display = "none";
+		countExperts = 0;
+	}
+
+	if (countExperts < 0) {
+		experts1.style.display = "none";
+		experts2.style.display = "none";
+		experts3.style.display = "block";
+		countExperts = 2;
+	} 
+});
+ExpertsBtnRight.addEventListener('click', (pos) => {
+	countExperts++;
+	switch (countExperts) {
+		case 0: {
+			experts1.style.display = "none";
+			experts2.style.display = "block";
+			experts3.style.display = "none";
+			break;
+		}
+
+		case 1: {
+			experts1.style.display = "none";
+			experts2.style.display = "none";
+			experts3.style.display = "block";
+			break;
+		}
+
+		case 2: {
+			experts1.style.display = "block";
+			experts2.style.display = "none";
+			experts3.style.display = "none";
+			break;
+		}
+	}
+	if (countExperts > 3) {
+		experts1.style.display = "none";
+		experts2.style.display = "block";
+		experts3.style.display = "none";
+		countExperts = 0;
+	}
+
+	if (countExperts < 0) {
+		investers1.style.display = "none";
+		investers2.style.display = "none";
+		investers3.style.display = "block";
+		countExperts = 2;
+	} 
 });
 //
